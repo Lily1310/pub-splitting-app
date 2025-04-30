b2269f5a37568c7e5298d458c2f8a7ac2087a1dc
Myat Noe Thu (Lily) <133743711+Lily1310@users.noreply.github.com>
2025-04-30 16:45:44 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,17 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-      caches.open('pub-splitter-cache').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/sw.js',
-          '/icon-192.png',
-          '/icon-512.png'
-        ]);
-      })
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('pub-splitter-cache')
+        .then(cache => {
+          return cache.addAll([
+            'index.html',
+            'manifest.json',
+            'sw.js',
+            'icon-192.png',
+            'icon-512.png'
+          ]);
+        })
+        .catch(err => {
+          console.error('Cache addAll failed:', err);
+        })
     );
   });
-  
-  self.addEventListener('fetch', e => {
-    e.respondWith(
-      caches.match(e.request).then(response => response || fetch(e.request))
-    );
-  });
-  
